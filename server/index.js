a9a5b2d8bd03cf1a8867393ad2d86b0921fa2088
Jenny Hou <jhou2012@gmail.com>
2021-03-24 22:30:50 -0400

    refactored server endpoints to async await

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
-// const db = require('../database/mongo.js') // USE FOR MONGO
+const db = require('../database/mongo.js') // USE FOR MONGO
 // const db = require('../database/sequelize.js') // USE FOR SEQUELIZE
-const db = require('../database/mysql.js') // USE FOR MYSQL
 const api = require('./github.js')
 const port = 3000
 
@@ -12,65 +11,43 @@ app.use(express.urlencoded({extended: true}))
 
 app.get('/get', async (req, res) => {
   try {
-      const results = await db.top25();
-      console.log('server test', results)
-      return res.status(200).json(results);
-  } catch (error) {
-      console.log(error);
-  }
+      let results = await db.top25();
+      res.status(200).send(results);
+  } catch (e) {
+      console.lot('server get error', e)
+      res.status(404).send('server get error')
+}
 });
 
-// app.get('/get', async (req,res) => {
-//   try {
-//     let data = await db.top25()
-//     console.log('server test', data)
-//     res.send(data)
-//   } catch(e) {
-//     console.log('server get error')
-//   }
-// })
-// app.get('/get', (req, res) => {
-//   return db.top25()
-//   .then(data => {
-//     console.log('server test', data)
-//     res.status(200).send(data)
-//   })
-//   .catch(err => {
-//     res.status(404).send('server get error')
-//   })
-// })
-
-app.post('/add', (req, res) => {
-  return api.getReposByUsername(req.body.search)
-  .then(response => {
-    return db.create(response)
-  })
-  .then(results => {
+app.post('/add', async (req, res) => {
+  try {
+    let apiData = await api.getReposByUsername(req.body.search)
+    let results = await db.create(apiData)
     res.status(200).send(results)
-  })
-  .catch(err => {
-    res.status(404).send('server post error')
-  })
+  } catch(e) {
+    console.log('server add error',e)
+    res.status(404).send('server add error')
+  }
 })
 
-app.put('/update', (req, res)=> {
-  db.update(req.body)
-  .then(data => {
-    res.status(200).send(data)
-  })
-  .catch(err => {
+app.put('/update', async (req, res) => {
+  try {
+    let results = await db.update(req.body)
+    res.status(200).send(results)
+  } catch(e) {
+    console.log('server update error',e)
     res.status(404).send('server update error')
-  })
+  }
 })
 
-app.delete('/del', (req, res) => {
-  db.del(req.body.repoId)
-  .then(data => {
-    res.status(200).send(data)
-  })
-  .catch(err => {
+app.delete('/del', async (req, res) => {
+  try {
+    let results = await db.del(req.body.repoId)
+    res.status(200).send(results)
+  } catch(e) {
+    console.log('server delete error:',e)
     res.status(404).send('server delete error')
-  })
+  }
 })
 
 app.listen(port, () => {
